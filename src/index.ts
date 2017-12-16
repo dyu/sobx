2df6016eefcdb0229ff87ea55b9fac7cab967286
dyu <david.yu.ftw@gmail.com>
2017-12-16 17:00:01 +0800

    array/scalar

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,70 @@
 import S from 's-js'
 import SArray from 's-array'
 
-function makeReactive(obj: any, key: string, val: any) {
-    if (Array.isArray(val)) {
-        obj[key] = SArray(val)
-        return
-    } else if (typeof val === 'function') {
-        return
-    }
-    
-    const property = Object.getOwnPropertyDescriptor(obj, key)
-    if (property && property.configurable === false) {
-        return
-    }
-    
-    if (typeof val === 'object') {
-        observable(val)
-        return
-    }
-    
-    // cater for pre-defined getter/setters
-    const getter = property && property.get
-    const setter = property && property.set
-    const s = S.data(val)
+function throwCannotSet(p) {
+    throw 'Cannot set'
+}
 
+function defArray(obj, key, val, $) {
+    var d
+    Object.defineProperty(obj, key, d={
+        enumerable: key !== '_',
+        configurable: true,
+        set: (p) => Array.isArray(p) ? val(p) : (p !== val && val(p())),
+        get: () => val
+    })
+    Object.defineProperty($, key, d)
+}
+
+function defScalar(obj, key, val, $) {
+    const s = S.data(val)
     Object.defineProperty(obj, key, {
         enumerable: key !== '_',
         configurable: true,
-        //get: () => isArray ? s : s(),
-        get: s,
-        set: s
+        set: s,
+        get: s
+    })
+    
+    const g = () => s()
+    var v
+    Object.defineProperty($, key, {
+        enumerable: key !== '_',
+        configurable: true,
+        set: throwCannotSet,
+        get: () => v !== undefined ? v : (v = S(g))
     })
 }
 
-/**
- * Make the object observable.
- */
-export function observable<T>(obj: T): T {
-    const keys = Object.keys(obj)
-    for (let i = 0; i < keys.length; i++) {
-        makeReactive(obj, keys[i], obj[keys[i]])
+function makeReactive(obj: any, key: string, val: any, $: any) {
+    var pd
+    if (typeof val === 'function' || (pd = Object.getOwnPropertyDescriptor(obj, key)) && pd.configurable === false) {
+        $[key] = val
+    } else if (Array.isArray(val)) {
+        defArray(obj, key, SArray(val), $)
+    } else if (typeof val === 'object') {
+        observe(val, ($[key] = {}))
+    } else {
+        defScalar(obj, key, val, $)
     }
-    
+}
+
+function observe<T>(obj: T, $: any): T {
+    for (let key of Object.keys(obj)) {
+        makeReactive(obj, key, obj[key], $)
+    }
+    Object.defineProperty(obj, '$', {
+        enumerable: false,
+        writable: false,
+        value: $
+    })
     return obj
+}
+
+/**
+ * Make the object/pojo observable.
+ */
+export function observable<T>(obj: T, $?: any): T {
+    return observe(obj, $ || {})
 }
 
 /**
@@ -53,18 +75,22 @@ export function bindTo(obj, fn: Function) : Function {
 }
 
 /**
- * Ignore this.
+ * Resolves the property value.
  */
-export function prop<T>(array: any, def: T): T {
-    if (!Array.isArray(array))
-        return array === undefined ? def : array
+export function prop<T>(val: any, def: T): T {
+    if (typeof val === 'function')
+        return val()
+    if (!Array.isArray(val))
+        return val === undefined ? def : val
     
-    switch (array.length) {
-        case 1: return array[0]
-        case 2: return array[0][array[1]]
-        case 3: return array[0][array[1]] ? array[2] : def
-        case 4: return array[0][array[1]] ? array[2] : array[3]
+    switch (val.length) {
+        case 1: return val[0]
+        case 2: return val[0][val[1]]
+        case 3: return val[0][val[1]] ? val[2] : def
+        case 4: return val[0][val[1]] ? val[2] : val[3]
         default: return def
     }
 }
+
+
 
